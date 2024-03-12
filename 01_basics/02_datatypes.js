cc95eafe67c2de038b6ce5d9aa054c83dd930b48
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-03-12 13:56:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+console.log(3+3);
+console.log("Mohit");
+
+let name="Mohit";
+let age="23";
+let isLoggedIn= false;
+let state = null;
+
+// number => 2 to power 53   |
+// bigint                    |
+// string => ""              |  DATA TYPES
+// boolean => true/false     |  
+// null => standalone value                   |
+// undefined
+// symbol => unique
+console.log(typeof("Mohit"));
+console.log(typeof(isLoggedIn));
+console.log(typeof(null));
