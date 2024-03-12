0c57750a2908db48edd0c6387a5ee6e824537fec
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-03-12 20:36:32 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score= "33abc"
+//console.log(typeof score)
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+let val= null
+//console.log(typeof val)
+//console.log(Number(val))
+// "33"=> 33
+// "33abc"=> NaN
+// true=>1; fale=>0
+
+let isLoggedIn="Mohit"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+ // 1=> true;
+ // 0=> false
+ // "hitesh"=> true
+
+ let someNumber= 33
+ let stringNumber= String(someNumber)
+ console.log(typeof stringNumber)
+ console.log(stringNumber)
