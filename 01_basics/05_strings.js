541bf47ccfe0c0e35f908db86a4cd1a4709b3bb7
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-03-20 14:39:45 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name= "Mohit";
+const repocount= 50;
+// console.log(name+repocount)
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+const gameName = new String("Mohit");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
