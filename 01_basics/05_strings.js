707aa9f9bc75e46ba587575885a2089e1da20bbe
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-03-21 18:17:14 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,10 +2,21 @@ const name= "Mohit";
 const repocount= 50;
 // console.log(name+repocount)
 console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
-const gameName = new String("Mohit");
+const gameName = new String("Mohit-singh-rathore");
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+const newString= gameName.substring(0,4);
+console.log(newString);
+const newStringone= "   Mohit   ";
+console.log(newStringone);
+console.log(newStringone.trim());
+const url= "https://www.cricbuzz.com/cricket-schedule/upcoming-series/international"
+console.log(url.replace("-","$"));
+console.log(url.includes('chfjggk'));
+console.log(gameName.split('-'));
+
 
