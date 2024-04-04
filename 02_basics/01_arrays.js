9676424339c745d264f6c5dbf5ec90cc964ede24
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-04 20:07:21 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+//array 
+const myarray= [1,2,3,4,5,6,7]
+const myheros= ["Shushant","Ajay"]
+
+const myarray2= new Array(1,5,6,4,6)
+// console.log(myarray[1])
+
+//Array methods 
+// myarray.push(8)
+// myarray.push(9)
+// myarray.pop()
+// myarray.unshift(0)
+// console.log(myarray.includes(9))
+// console.log(myarray.indexOf(91))
+// const newarray= myarray.join()
+
+// console.log(myarray)
+// console.log(newarray)
+// console.log(typeof newarray)
+
+//Slice and Splice
+console.log("A", myarray)
+const myn1= myarray.slice(2,6)
+console.log(myn1)
+console.log("B",myarray)
+
+const myn2= myarray.splice(3,2)
+console.log(myn2)
+console.log("C",myarray)
+
+
