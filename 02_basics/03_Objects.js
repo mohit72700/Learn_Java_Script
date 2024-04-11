aa71a3926ef388e5706ff8d12cab1ba5821bb6c0
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-11 10:47:44 +0000

    Object

---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,19 @@
+// Singleton
+
+// Object literals
+
+const user1= {
+     name:"Mohit",
+     "full name": "Mohit singh Rathore",
+     age: 24,
+     location: "Rourkela",
+     email: "mohit@ffgh.com",
+     isLoggedIn:false,
+     lastLoginDay: ["Monday","Wendnesday"]
+
+}
+console.log(user1.name);
+console.log(user1["age"]);
+console.log(user1["full name"]);
+
+
