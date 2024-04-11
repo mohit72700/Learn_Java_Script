82110c80448a461b7b67ced96b09eee2bab1194f
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-11 15:55:30 +0000

    object

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -2,10 +2,13 @@
 
 // Object literals
 
+const mySym= Symbol("Key1")
+
 const user1= {
      name:"Mohit",
      "full name": "Mohit singh Rathore",
      age: 24,
+     [mySym]: "My symbol", //if we want to use mySym as a symbol then we need to close mySym in [].
      location: "Rourkela",
      email: "mohit@ffgh.com",
      isLoggedIn:false,
@@ -15,5 +18,10 @@ const user1= {
 console.log(user1.name);
 console.log(user1["age"]);
 console.log(user1["full name"]);
+console.log(user1[mySym]);
+
+//you can update the value of key inside the object untill you do not freez the object 
+user1.email= "mohit8764@gmail.com"
+
 
 
