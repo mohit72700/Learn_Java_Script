ee9fcf4a0dba26a375a7e566778ac4d88afe2fa5
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-11 19:06:24 +0000

    objects

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -15,13 +15,31 @@ const user1= {
      lastLoginDay: ["Monday","Wendnesday"]
 
 }
-console.log(user1.name);
-console.log(user1["age"]);
-console.log(user1["full name"]);
-console.log(user1[mySym]);
+// console.log(user1.name);
+// console.log(user1["age"]);
+// console.log(user1["full name"]);
+// console.log(user1[mySym]);
 
 //you can update the value of key inside the object untill you do not freez the object 
 user1.email= "mohit8764@gmail.com"
+console.log(user1.email)
+//Let's freez the object
+// Object.freeze(user1)
+user1.email= "mohit@microsoft.com"
+console.log(user1.email)
+// console.log(user1)
+
+//If we want to use below function then first we need to check that object should not be freez
+user1.greeting= function(){
+     console.log("Hello Java script users");
+}
+user1.greeting2 = function(){
+     console.log(`Hello Java script user, ${this.name}`);
+}
+console.log(user1.greeting());
+console.log(user1.greeting2());
+
+
 
 
 
