e64a1dcb1fb90b8d1f9298fb44f3d5459a0aea6f
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-16 07:22:39 +0000

    scope, scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,9 +6,39 @@ if(true)
     const b=20
     // var c= 30
     let c =40
-    console.log("Inner value of a: ",a)
+    // console.log("Inner value of a: ",a)
 
 }
-console.log("Outer value of a: ",a);
+// console.log("Outer value of a: ",a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Mohit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)
+
+    // two()
+}
+
+// one()
+
+// ++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+console.log(addone(7))    //It will not give any error
+function addone(num){
+    return num+1;
+}
+
+console.log(addone(5))  //It will not give any error
+
+// console.log(addTwo(5))   //....This line will give error.... 
+const addTwo = function(num){
+    return num+2;
+}
+
+console.log(addTwo(5))  //It will not give any error
