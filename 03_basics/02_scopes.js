58c4bf6beb19af752915bf8515978e061bb8f78a
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-16 06:45:26 +0000

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+// var c= 400
+let a = 300
+if(true)
+{
+    let a=10
+    const b=20
+    // var c= 30
+    let c =40
+    console.log("Inner value of a: ",a)
+
+}
+console.log("Outer value of a: ",a);
+// console.log(b);
+// console.log(c);
