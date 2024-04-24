5ffdb029c34e00b0a31a3d8f520d1cfec6a49f3f
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2024-04-24 20:01:29 +0530

    While and DoWhile loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// While
+
+let index = 0;
+while(index<=10)
+{
+  // console.log(`Value of index is ${index}`);
+  index+=2;
+}
+
+let myArray = ["Mohit", "Rohan", "Rohit", "Gaurav"]
+let arr = 0;
+while(arr < myArray.length) {
+  // console.log(`Value is: ${myArray[arr]}`)
+  arr+=1
+}
+
+//Do While loop
+let score = 14
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while(score<=12)
