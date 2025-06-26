00899e3280944ef0001758e5073b8af5c5db4d22
Mohit Singh Rathore <92523950+mohit72700@users.noreply.github.com>
2025-06-27 02:34:48 +0530

    update

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,7 +2,7 @@ const name= "Mohit";
 const repocount= 50;
 // console.log(name+repocount)
 console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
-const gameName = new String("Mohit-singh-rathore");
+const gameName = new String("Mohit*singh*rathore");
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
@@ -17,6 +17,6 @@ console.log(newStringone.trim());
 const url= "https://www.cricbuzz.com/cricket-schedule/upcoming-series/international"
 console.log(url.replace("-","$"));
 console.log(url.includes('chfjggk'));
-console.log(gameName.split('-'));
+console.log(gameName.split('*'));
 
 
